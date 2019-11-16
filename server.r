baaf9832d62efb0b15e08701f02fe46782f24d38
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2019-11-16 13:47:30 -0800

    initial commit

--- server.r
+++ server.r
@@ -5,9 +5,16 @@ library(lubridate)
 library(dplyr)
 library(ggplot2)
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   police_killings <- read.csv("data/fatal-police-shootings-data.csv",
                               stringsAsFactors = FALSE)
+  
+  observe({
+    updateCheckboxGroupInput(
+      session, "races", choices = choices,
+      selected = if (input$bar == "All") choices
+    )
+  })
   
   killings_by_state <- police_killings %>% 
     group_by(state)

e7945d94b704638979f86bd564166ee6a1f3a290
kimikoboswell <ksb35@uw.edu>
2019-11-19 23:03:22 -0800

    made more progress on conclusion- need to make table and get individual reflections

--- ui.r
+++ ui.r
@@ -53,7 +53,6 @@ mainPanel(
 )
 )
 )
-shinyApp(ui = "ui.R", server = "server.R")
 
 background_page <- tabPanel(
   "Background",
@@ -92,7 +91,7 @@ mainPanel(
   img(src = "https://fresnoalliance.com/wp-content/uploads/2015/01/15014376865_930e981546_b.jpg"
       , height = "50%", width = "50%", align = "center"),
   p("Image by ep_jhu via Flickr Creative Commons"),
-  h2("Reserach Questions"),
+  h2("Research Questions"),
   p("1. What is the racial breakdown of police shooting victims?"),
   p("2. Is there a disparity between races killed by police?"),
   p("3. Which region has the highest amount of shootings?"),
@@ -130,17 +129,82 @@ visualizations_page <- tabPanel(
     )
     )
   )
-
+# for following section, need to add photos
 conclusion_page <- tabPanel(
-  "Conclusion"
+  "Conclusion",
+  h1 ("Conclusion"),
+  h5 ("This project gave us an opportunity
+      to work on our coding and teamwork skills.
+      With that said, it definetly had it's ups
+      and downs. "),
+  h3("Strengths"),
+  h5("-We were able to divide tasks based on what
+     each of us was good on. Tung was very helpful
+     with visualization creation. Autumn worked extensively
+     to create a functioning website. Kimiko went through and 
+     filled in the back end of the report, as well as 
+     update the proposal to reflect our current project."),
+  h5("-Despite having very different schedules, we all managed
+     to meet and finish our assigned tasks"),
+  h3("Weaknesses"),
+  h5("-Our original dataset and thesis were faulty and we had to
+     play a game of catch up, trying to find a new dataset."),
+  h5("-We all had different schedules, and it was hard to find
+     times were we could all meet."),
+  h5("-Creating a substantial visualization was hard because we were
+     missing information to make the type of visualization we wanted to
+     make at first."),
+  h3("Lessons"),
+  h5("We learned quite a bit from doing this project. We learned how 
+     data can be hard to manipulate, and how it is important to find 
+     datasets which fit our overall plan. We also learned the importance
+     of communicating when merging, especially if on the same branch. 
+     Finally, we learned the importance about consistent communication
+     to make sure everyone is getting their part done and that no 
+     question remain unasked."),
+  h3("Future Work"),
+  h5("There is a lot of potential for our work to 
+     be used in the future by us or by someone else. We simply
+     highlighted aspects of Police Brutality within the U.S.
+     for one year. Someone could analyze it for multiple years
+     and maybe pull a conclusion about the fluctuation ins police 
+     brutality rates. They could then take it a step further
+     by adding the real world context to explain the fluctuations
+     in police shootings."),
+  h2("References"),
+  h5("Fatal Force: 2018 Police Shootings Database.,
+     The Washington Post, WP Company,
+     https://www.washingtonpost.com/graphics/
+     2018/national/police-shootings-2018/."),
 )
-
+#Input table or representation of all columnnames
 tech_page <- tabPanel(
-  "About the Tech"
+  "About the Tech (Appendix 1)",
+  
+  
 )
-
+#Find a way to use photos from files
 us_page <- tabPanel(
-  "About Us"
+  "About Us",
+  h1("Team Members"),
+  h3("Autumn Rausch"),
+  h5("Freshman studying
+     Informatics, and Criminology"),
+  #following header is for Autumn's bio
+  h5(""),
+  h3("Kimiko Boswell"),
+  h5("Senior studying International
+     Studies, Immigration, and Informatics"),
+  h5("After doing this assignment, I learned how to
+     effectively create a website using shiny and utilize
+     datasources to come to conclusions. I was really frustrated
+     with the merging process, but it felt so satisfying when
+     the website worked and I got to see all the hard work me and 
+     especially my teammates put in."),
+  h3("Tung Netmaneesuk"),
+  h5("Junior studying Psychology"),
+  #following header is for Tung's bio
+  h5(""),
 )
 
 ui <- navbarPage(
